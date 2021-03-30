3fd6079842a8c0c177f2d6620278631d4cf8323c
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-03-30 08:48:26 +0600

    deploy heroku

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config();
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+const port = 5000
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.muahx.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const productsCollection = client.db("emaJohnStore").collection("products");
+    const ordersCollection = client.db("emaJohnStore").collection("orders");
+    app.post('/addProduct',(req, res)=>{
+        const products = req.body;
+        productsCollection.insertOne(products)
+        .then(result =>{
+            res.send(result.insertedCount)
+        })
+    })
+    app.get('/products',(req, res)=>{
+        productsCollection.find({})
+        .toArray((err, documents)=>{
+            res.send(documents)
+        })
+    })
+    app.get('/product/:key',(req, res)=>{
+        productsCollection.find({key: req.params.key})
+        .toArray((err, documents)=>{
+            res.send(documents[0])
+        })
+    })
+    app.post('/productsByKeys',(req, res)=>{
+        const proudctkeys = req.body;
+        productsCollection.find({key: {$in: proudctkeys}})
+        .toArray((err, documents)=>{
+            res.send(documents)
+        })
+    })
+    app.post('/addOrder',(req, res)=>{
+        const order = req.body;
+        ordersCollection.insertOne(order)
+        .then(result =>{
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
